367b091154985ed5b4eef53ec30692521cf73c0c
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2017-11-08 00:42:12 +0100

    push latest changes

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'opdracht.dev'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'webdevelopment'),
-            'username' => env('DB_USERNAME', 'yawuar'),
-            'password' => env('DB_PASSWORD', 'Adobexd2'),
+            'database' => env('DB_DATABASE', 'u13191p9416_deliveroo'),
+            'username' => env('DB_USERNAME', 'u13191p9416_yawuar'),
+            'password' => env('DB_PASSWORD', 'Adobeedge2'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
