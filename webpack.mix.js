14fbea29a1f124ae987e99519e4c06c7445d6972
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2017-11-08 23:53:56 +0100

    fixed user disqualify

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,7 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .sass('resources/assets/sass/admin.scss', 'public/css');
+
+mix.options({
+  processCssUrls: false // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
+});
